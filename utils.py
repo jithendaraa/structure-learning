13d596a04b8dc6c5de251cadeb019b61bdba1025
jithendaraa <jithen.subra@gmail.com>
2021-12-12 15:51:46 -0500

    minor changes

--- utils.py
+++ utils.py
@@ -222,7 +222,7 @@ def set_tb_logdir(opt):
   elif opt.model in ['VAE_DIBS']:
     logdir += f'_({opt.num_nodes})_seed{opt.seed}_{opt.data_seed}_proj{opt.proj}{opt.proj_dims}_samples{opt.num_samples}_expedges{opt.exp_edges}_sftcnstrnt_{opt.soft_constraint}_dibsupdates{opt.num_updates}_knownED{opt.known_ED}'
   elif opt.model in ['Decoder_DIBS']:
-    logdir += f'_({opt.num_nodes})_seed{opt.seed}_{opt.data_seed}_proj{opt.proj}{opt.proj_dims}_samples{opt.num_samples}_expedges{opt.exp_edges}_sftcnstrnt_{opt.soft_constraint}_steps{opt.steps}_dibsupdates{opt.num_updates}_knownED{opt.known_ED}_lindecode{opt.linear_decoder}_algo{opt.algo}_{opt.z_prior}_z_prior'
+    logdir += f'_({opt.num_nodes})_seed{opt.seed}_{opt.data_seed}_proj{opt.proj}{opt.proj_dims}_samples{opt.num_samples}_expedges{opt.exp_edges}_sftcnstrnt_{opt.soft_constraint}_steps{opt.steps}_dibsupdates{opt.num_updates}_knownED{opt.known_ED}_lindecode{opt.linear_decoder}_algo{opt.algo}_{opt.z_prior}_z_prior_particles{opt.n_particles}_beta{opt.beta}'
 
   print("logdir:", logdir)
   return logdir
