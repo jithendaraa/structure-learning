848e1f47ea530a130c849b0f0a23a09b7f7891fb
jithendaraa <jithen.subra@gmail.com>
2022-02-06 23:34:18 -0500

    remove obs data gen from VCN ER datagen

--- exps/datagen.py
+++ exps/datagen.py
@@ -6,17 +6,6 @@ from os.path import join
 import numpy as np
 from jax import numpy as jnp
 
-def generate_data(opt, logdir):
-    train_dataloader = ER(num_nodes = opt.num_nodes, exp_edges = opt.exp_edges, 
-                            noise_type = opt.noise_type, noise_sigma = opt.noise_sigma, 
-                            num_samples = opt.num_samples, mu_prior = opt.theta_mu, 
-                            sigma_prior = opt.theta_sigma, seed = opt.data_seed, 
-                            project=opt.proj, proj_dims=opt.proj_dims, noise_mu=opt.noise_mu)
-
-    nx.draw(train_dataloader.graph, with_labels=True, font_weight='bold', node_size=1000, font_size=25, arrowsize=40, node_color='#FFFF00') # save ground truth graph
-    plt.savefig(join(logdir,'gt_graph.png'))
-
-    return train_dataloader
 
 def generate_interv_data(opt, n_intervention_sets, target):
     data_ = []
