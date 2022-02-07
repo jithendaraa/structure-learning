Fix metrics calculation

exps/dibs_new/dibs/metrics.py
@@ -1,5 +1,6 @@
 import jax.numpy as jnp
 from jax.scipy.special import logsumexp
+import numpy as onp
 
 from dibs_new.dibs.utils.tree import tree_mul, tree_select
 from dibs_new.dibs.graph_utils import elwise_acyclic_constr_nograd
@@ -53,7 +54,7 @@ def pairwise_structural_hamming_distance(*, x, y):
     return shd
 
 
-def expected_shd(*, dist, g):
+def expected_shd(*, dist, g, unique=False):
     """
     Computes expected structural hamming distance metric, defined as
 
@@ -68,14 +69,20 @@ def expected_shd(*, dist, g):
     """
     n_vars = g.shape[0]
 
+    if unique:
+        unique_gs, _ = onp.unique(dist.g, axis=0, return_counts=True)
+        is_dag = elwise_acyclic_constr_nograd(unique_gs, n_vars) == 0
+        particles = unique_gs[is_dag, :, :]
+    else:
+        is_dag = elwise_acyclic_constr_nograd(dist.g, n_vars) == 0
+        particles = dist.g[is_dag, :, :]
+
     # select acyclic graphs
-    is_dag = elwise_acyclic_constr_nograd(dist.g, n_vars) == 0
+    log_weights = dist.logp[is_dag] - logsumexp(dist.logp[is_dag])
+
     if is_dag.sum() == 0:
         # score as "wrong on every edge"
         return n_vars * (n_vars - 1) / 2
-    
-    particles = dist.g[is_dag, :, :]
-    log_weights = dist.logp[is_dag] - logsumexp(dist.logp[is_dag])
     
     # compute shd for each graph
     shds = pairwise_structural_hamming_distance(x=particles, y=g[None]).squeeze(1)
@@ -128,7 +135,7 @@ def expected_edges(*, dist):
     return edges
 
 
-def threshold_metrics(*, dist, g):
+def threshold_metrics(*, dist, g, unique=False):
     """
     Computes various threshold metrics (e.g. ROC, precision-recall, ...)
 
@@ -142,8 +149,16 @@ def threshold_metrics(*, dist, g):
     n_vars = g.shape[0]
     g_flat = g.reshape(-1)
 
+    if unique:
+        unique_gs, _ = onp.unique(dist.g, axis=0, return_counts=True)
+        is_dag = elwise_acyclic_constr_nograd(unique_gs, n_vars) == 0
+        particles = unique_gs[is_dag, :, :]
+
+    else:
+        is_dag = elwise_acyclic_constr_nograd(dist.g, n_vars) == 0
+        particles = dist.g[is_dag, :, :]
+
     # select acyclic graphs
-    is_dag = elwise_acyclic_constr_nograd(dist.g, n_vars) == 0
     if is_dag.sum() == 0:
         # score as random/junk classifier
         # for AUROC: 0.5
@@ -154,7 +169,6 @@ def threshold_metrics(*, dist, g):
             'ave_prec': (g.sum() / (n_vars * (n_vars - 1))).item(),
         }
 
-    particles = dist.g[is_dag, :, :]
     log_weights = dist.logp[is_dag] - logsumexp(dist.logp[is_dag])
 
     # P(G_ij = 1) = sum_G w_G 1[G = G] in log space
@@ -185,7 +199,7 @@ def threshold_metrics(*, dist, g):
     }
 
 
-def neg_ave_log_marginal_likelihood(*, dist, eltwise_log_marginal_likelihood, x):
+def neg_ave_log_marginal_likelihood(*, dist, eltwise_log_marginal_likelihood, x, unique=False):
     """
     Computes neg. ave log marginal likelihood for a marginal posterior over :math:`G`, defined as
 
@@ -205,15 +219,22 @@ def neg_ave_log_marginal_likelihood(*, dist, eltwise_log_marginal_likelihood, x)
     """
     n_ho_observations, n_vars = x.shape
 
-    # select acyclic graphs
-    is_dag = elwise_acyclic_constr_nograd(dist.g, n_vars) == 0
+    if unique:
+        unique_gs, _ = onp.unique(dist.g, axis=0, return_counts=True)
+        is_dag = elwise_acyclic_constr_nograd(unique_gs, n_vars) == 0
+        particles = unique_gs[is_dag, :, :]
+        g = unique_gs[is_dag, :, :]
+
+    else:
+        is_dag = elwise_acyclic_constr_nograd(dist.g, n_vars) == 0
+        g = dist.g[is_dag, :, :]
+
     if is_dag.sum() == 0:
         # score as empty graph only
         g = jnp.zeros((1, n_vars, n_vars), dtype=dist.g.dtype)
         log_weights = jnp.array([0.0], dtype=dist.logp.dtype)
 
     else:
-        g = dist.g[is_dag, :, :]
         log_weights = dist.logp[is_dag] - logsumexp(dist.logp[is_dag])
         
     log_likelihood = eltwise_log_marginal_likelihood(g, x)

exps/dibs_new/dibs/target.py
@@ -72,10 +72,11 @@ def make_synthetic_bayes_net(*,
     """
 
     # remember random key
-    passed_key = key.copy()
+    key, rng = random.split(key)
 
     # generate ground truth observations
     key, subk = random.split(key)
+    print("subk", subk)
     g_gt = graph_dist.sample_G(subk)
     g_gt_mat = jnp.array(graph_to_mat(g_gt))
 
@@ -106,7 +107,7 @@ def make_synthetic_bayes_net(*,
 
     # return and save generated target object
     data = Data(
-        passed_key=passed_key,
+        passed_key=key,
         n_vars=n_vars,
         n_observations=n_observations,
         n_ho_observations=n_ho_observations,
@@ -150,8 +151,8 @@ def make_graph_model(*, n_vars, graph_prior_str, edges_per_node=1):
 
 
 def make_linear_gaussian_equivalent_model(*, key, n_vars=20, graph_prior_str='sf', 
-    obs_noise=0.1, mean_edge=0.0, sig_edge=1.0, n_observations=100,
-    n_ho_observations=100):
+    edges_per_node=1, obs_noise=0.1, mean_edge=0.0, 
+    sig_edge=1.0, n_observations=100, n_ho_observations=100):
     """
     Samples a synthetic linear Gaussian BN instance 
     with Bayesian Gaussian equivalent (BGe) marginal likelihood 
@@ -176,7 +177,9 @@ def make_linear_gaussian_equivalent_model(*, key, n_vars=20, graph_prior_str='sf
     """
 
     # init models
-    graph_dist = make_graph_model(n_vars=n_vars, graph_prior_str=graph_prior_str)
+    graph_dist = make_graph_model(n_vars=n_vars, 
+                                graph_prior_str=graph_prior_str,
+                                edges_per_node=edges_per_node)
 
     generative_model = LinearGaussian(
         obs_noise=obs_noise, mean_edge=mean_edge, 