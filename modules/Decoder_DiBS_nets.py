bbe54ab8fe30f3acc2626d9cffffc9d7bc94b108
jithendaraa <jithen.subra@gmail.com>
2022-02-08 13:52:07 -0500

    Add Decoder net and z_mu_logvar net

---
+++ modules/Decoder_DiBS_nets.py
@@ -0,0 +1,59 @@
+from flax import linen as nn
+
+class Z_mu_logvar_Net(nn.Module):
+    latent_dims: int
+    num_cholesky_terms: int
+
+    @nn.compact
+    def __call__(self, g):
+        z = nn.Dense(20, name='encoder_0')(g)
+        z = nn.relu(z)
+        z = nn.Dense(3 * self.latent_dims, name='encoder_1')(z)
+        z = nn.relu(z)
+        z = nn.Dense(3 * self.latent_dims, name='encoder_2')(z)
+        z = nn.relu(z)
+        
+        z_mu = nn.Dense(3 * self.latent_dims, name='mu_encoder_0')(z)
+        z_mu = nn.relu(z_mu)
+        z_mu = nn.Dense(3 * self.latent_dims, name='mu_encoder_2')(z_mu)
+        z_mu = nn.relu(z_mu)
+        z_mu = nn.Dense(3 * self.latent_dims, name='mu_encoder_3')(z_mu)
+        z_mu = nn.relu(z_mu)
+        z_mu = nn.Dense(self.latent_dims, name='mu_encoder_1')(z_mu)
+
+        z_logcholesky = nn.Dense(self.latent_dims*self.latent_dims, name='logcovar_encoder_0')(z)
+        z_logcholesky = nn.relu(z_logcholesky)
+        z_logcholesky = nn.Dense(self.latent_dims*self.latent_dims, name='logcovar_encoder_2')(z_logcholesky)
+        z_logcholesky = nn.relu(z_logcholesky)
+        z_logcholesky = nn.Dense(self.latent_dims*self.latent_dims, name='logcovar_encoder_3')(z_logcholesky)
+        z_logcholesky = nn.relu(z_logcholesky)
+        z_logcholesky = nn.Dense(self.num_cholesky_terms, name='logcovar_encoder_1')(z_logcholesky)
+
+        return jnp.asarray(z_mu), jnp.asarray(z_logcholesky)
+
+
+class Decoder(nn.Module):
+    dims: int
+    linear_decoder: bool
+
+    @nn.compact
+    def __call__(self, z):
+        if self.linear_decoder:
+            z = nn.Dense(self.dims, name='decoder_fc0')(z)
+        else:
+            z = nn.Dense(self.dims, name='decoder_fc0')(z)
+            z = nn.relu(z)
+            z = nn.Dense(self.dims, name='decoder_fc1')(z)
+            z = nn.relu(z)
+            z = nn.Dense(self.dims, name='decoder_fc2')(z)
+            z = nn.relu(z)
+            z = nn.Dense(self.dims, name='decoder_fc3')(z)
+            z = nn.relu(
+                z)
+            z = nn.Dense(self.dims, name='decoder_fc4')(z)
+            z = nn.relu(z)
+            z = nn.Dense(self.dims, name='decoder_fc5')(z)
+            z = nn.relu(z)
+            z = nn.Dense(self.dims, name='decoder_fc6')(z)
+            
+        return z
