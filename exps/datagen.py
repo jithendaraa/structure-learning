4e3750d6f214ac6ba1f59be6941ef1d90613a429
jithendaraa <jithen.subra@gmail.com>
2022-02-19 18:09:05 -0500

    Get x=5 data samples per node per interv set for n = 10 interv sets

--- exps/datagen.py
+++ exps/datagen.py
@@ -5,32 +5,42 @@ import matplotlib.pyplot as plt
 from os.path import join
 import numpy as np
 from jax import numpy as jnp
-import torch
+import torch, pdb
 from jax import random
 
-def generate_interv_data(opt, n_intervention_sets, target):
+def generate_interv_data(opt, n_interv_sets, target):
     data_ = []
     no_interv_targets = np.zeros((opt.num_samples, opt.num_nodes)).astype(bool) # observational
     data_start, data_end = opt.obs_data, opt.num_samples
+    
     num_interv_data = data_end - data_start
-    interv_data_per_set = int(num_interv_data / n_intervention_sets)    
+    interv_data_per_node = int(num_interv_data / opt.num_nodes)
+    interv_data_per_node_per_set = int(interv_data_per_node / n_interv_sets)
+    interv_data_per_set = int(interv_data_per_node_per_set * opt.num_nodes)    
     interv_data = np.array(target.x_interv)
 
     if num_interv_data > 0:
-        for i in range(n_intervention_sets):
-            interv_target = np.array([False] * opt.num_nodes)
-            intervened_node = [*interv_data[i][0]][0]
-            interv_target[intervened_node] = True
-            start_idx, end_idx = int(data_start + (i * interv_data_per_set)), int(data_start + ((i+1) * interv_data_per_set)), 
-            no_interv_targets[start_idx : end_idx, :] = interv_target[None, :]
-            data_.append(interv_data[i][1][:interv_data_per_set])
+        for i in range(n_interv_sets):
+            interv_targets = []
+            for j in range(opt.num_nodes):
+                interv_target = np.array([False] * opt.num_nodes).reshape(1, -1).repeat(interv_data_per_node_per_set, axis=0)
+                interv_target[:, j] = True
+                data_.append(interv_data[j][1][i*interv_data_per_node_per_set: (i+1)*interv_data_per_node_per_set])
+                interv_targets.append(interv_target)
+            interv_targets = np.array(interv_targets).reshape(interv_data_per_set, opt.num_nodes)
+            no_interv_targets[data_start+i*interv_data_per_set : data_start+(i+1)*interv_data_per_set] = interv_targets
 
-    data_, no_interv_targets = jnp.array(data_).reshape(-1, opt.num_nodes), jnp.array(no_interv_targets)
+    data_, no_interv_targets = jnp.array(data_).reshape(num_interv_data, opt.num_nodes), jnp.array(no_interv_targets)
     return data_, no_interv_targets
 
 def get_data(opt, n_intervention_sets, target):
     interv_data, no_interv_targets = generate_interv_data(opt, n_intervention_sets, target)
     obs_data = jnp.array(target.x)[:opt.obs_data]
+    
+    interv_data_per_node = int((opt.num_samples - opt.obs_data) / opt.num_nodes)
+    interv_data_per_node_per_set = int(interv_data_per_node / n_intervention_sets)
+    interv_data_per_set = int(interv_data_per_node_per_set * opt.num_nodes)    
+    
     x = jnp.concatenate((obs_data, interv_data), axis=0)
 
     if opt.proj == 'linear': 
@@ -43,14 +53,16 @@ def get_data(opt, n_intervention_sets, target):
         true_decoder = P
         projected_samples = x @ P
         print(f'Data matrix after linear projection from {opt.num_nodes} dims to {opt.proj_dims} dims: {projected_samples.shape}')  
-        sample_mean = np.mean(x, axis=0)
-        sample_covariance = jnp.array(torch.cov(torch.transpose(torch.tensor(np.array(x)), 0, 1)))
+        data = obs_data
+        sample_mean = np.mean(data, axis=0)
+        sample_covariance = jnp.array(torch.cov(torch.transpose(torch.tensor(np.array(data)), 0, 1)))
 
     return obs_data, interv_data, x, no_interv_targets, projected_samples, sample_mean, sample_covariance
 
-def gen_data_from_dist(rng, q_z_mu, q_z_covar, num_samples):
+def gen_data_from_dist(rng, q_z_mu, q_z_covar, num_samples, interv_targets):
     q_z_mu = jnp.expand_dims(q_z_mu, 0).repeat(num_samples, axis=0)
     q_z_covar = jnp.expand_dims(q_z_covar, 0).repeat(num_samples, axis=0)
     data = random.multivariate_normal(rng, q_z_mu, q_z_covar)
+    data = jnp.where(interv_targets, 0.0, data)
     return data
 
