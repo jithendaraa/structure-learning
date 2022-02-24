c8e3999dc14a72be1e5c2a0d8ceacefbe7e3d6a0
jithendaraa <jithen.subra@gmail.com>
2022-02-23 21:02:41 -0500

    Generate interv data single node or multi node

--- exps/datagen.py
+++ exps/datagen.py
@@ -6,41 +6,77 @@ from os.path import join
 import numpy as np
 from jax import numpy as jnp
 import torch, pdb
+from random import sample
 from jax import random
 
-def generate_interv_data(opt, n_interv_sets, target):
+
+
+def single_node_interv_data(opt, n_interv_sets, no_interv_targets, target):
+    """
+        For every `n_interv_sets`, randomly choose an index idx_i in 0, 1.... (opt.num_nodes-1) where n = 
+        Let n = interventional data points per set = num_interv_data / n_interv_sets 
+        For every set in `n_interv_sets`, we generate n data points  correcsponding to intervention on node idx_i
+        [TODO]
+    """
     data_ = []
-    no_interv_targets = np.zeros((opt.num_samples, opt.num_nodes)).astype(bool) # observational
+    num_interv_data = opt.num_samples - opt.obs_data
+    interv_data_pts_per_set = int(num_interv_data / n_interv_sets)
+    interv_data = np.array(target.x_interv)
+    assert num_interv_data <= len(target.x_interv[0][1])
+
+    for i in range(n_interv_sets):
+        interv_targets = []
+        idx_i = np.random.randint(0, opt.num_nodes)
+        no_interv_targets[opt.obs_data + i * interv_data_pts_per_set : opt.obs_data + (i+1) * interv_data_pts_per_set, idx_i] = True
+        data_idxs = sample(range(len(interv_data[idx_i][1])), interv_data_pts_per_set)
+        data_.append(interv_data[idx_i][1][np.array(data_idxs)])
+
+    return data_, no_interv_targets
+
+
+def multi_node_interv_data(opt, n_interv_sets, no_interv_targets, target):
+    """
+        Generates `n_interv_sets` sets of interventional data.
+        Each set has `interv_data_per_node_per_set` interv. data points per node equally spread
+        [TODO]
+    """
+    data_ = []
     data_start, data_end = opt.obs_data, opt.num_samples
-    
-    num_interv_data = data_end - data_start
+    num_interv_data = opt.num_samples - opt.obs_data
+    interv_data_per_set = int(num_interv_data / n_interv_sets)
     interv_data_per_node = int(num_interv_data / opt.num_nodes)
     interv_data_per_node_per_set = int(interv_data_per_node / n_interv_sets)
-    interv_data_per_set = int(interv_data_per_node_per_set * opt.num_nodes)    
     interv_data = np.array(target.x_interv)
 
-    if num_interv_data > 0:
-        for i in range(n_interv_sets):
-            interv_targets = []
-            for j in range(opt.num_nodes):
-                interv_target = np.array([False] * opt.num_nodes).reshape(1, -1).repeat(interv_data_per_node_per_set, axis=0)
-                interv_target[:, j] = True
-                data_.append(interv_data[j][1][i*interv_data_per_node_per_set: (i+1)*interv_data_per_node_per_set])
-                interv_targets.append(interv_target)
-            interv_targets = np.array(interv_targets).reshape(interv_data_per_set, opt.num_nodes)
-            no_interv_targets[data_start+i*interv_data_per_set : data_start+(i+1)*interv_data_per_set] = interv_targets
+    for i in range(n_interv_sets):
+        interv_targets = []
+        for j in range(opt.num_nodes):
+            interv_target = np.array([False] * opt.num_nodes).reshape(1, -1).repeat(interv_data_per_node_per_set, axis=0)
+            interv_target[:, j] = True
+            data_.append(interv_data[j][1][i*interv_data_per_node_per_set: (i+1)*interv_data_per_node_per_set])
+            interv_targets.append(interv_target)
+        interv_targets = np.array(interv_targets).reshape(interv_data_per_set, opt.num_nodes)
+        no_interv_targets[data_start+i*interv_data_per_set : data_start+(i+1)*interv_data_per_set] = interv_targets
 
+    return data_, no_interv_targets
+
+
+def generate_interv_data(opt, n_interv_sets, target):
+    no_interv_targets = np.zeros((opt.num_samples, opt.num_nodes)).astype(bool) # observational
+    num_interv_data = opt.num_samples - opt.obs_data
+
+    if num_interv_data > 0:
+        if opt.interv_type == 'single':
+            data_, no_interv_targets = single_node_interv_data(opt, n_interv_sets, no_interv_targets, target)
+        elif opt.interv_type == 'multi':
+            data_, no_interv_targets = multi_node_interv_data(opt, n_interv_sets, no_interv_targets, target)
+        
     data_, no_interv_targets = jnp.array(data_).reshape(num_interv_data, opt.num_nodes), jnp.array(no_interv_targets)
     return data_, no_interv_targets
 
 def get_data(opt, n_intervention_sets, target):
     interv_data, no_interv_targets = generate_interv_data(opt, n_intervention_sets, target)
     obs_data = jnp.array(target.x)[:opt.obs_data]
-    
-    interv_data_per_node = int((opt.num_samples - opt.obs_data) / opt.num_nodes)
-    interv_data_per_node_per_set = int(interv_data_per_node / n_intervention_sets)
-    interv_data_per_set = int(interv_data_per_node_per_set * opt.num_nodes)    
-    
     x = jnp.concatenate((obs_data, interv_data), axis=0)
 
     if opt.proj == 'linear': 
