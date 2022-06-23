664fb85c47291d45131072cbe3c7ca01ee65025d
yannadani <yashas.annadani@gmail.com>
2022-06-23 19:14:07 +0200

    local changes

--- scripts/linear_decoder_bcd_across_interv_job.sh
+++ scripts/linear_decoder_bcd_across_interv_job.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 #SBATCH --time=27:00:00
-#SBATCH --mem=32G
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=6
-#SBATCH --mail-user=jithen.subra@gmail.com
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-#SBATCH --mail-type=REQUEUE
-#SBATCH --mail-type=ALL
 
 seeds=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
 exp_edges=(1.0)
@@ -17,10 +9,9 @@ num_steps=(5000)
 
 num_samples=(3600)
 obs_data=300
-num_nodes=20
-proj_dims=100
+num_nodes=50
+proj_dims=5000
 off_wandb='False'
-exp_name='linear_decoder_bcd_across_intervs'
 
 identity_proj='False'
 proj_sparsity=0.0
@@ -59,15 +50,14 @@ echo "Script"
 
 # cp /home/jithen/scratch/datasets/CLEVR_v1.0.zip $SLURM_TMPDIR/CLEVR_v1.0.zip
 # unzip $SLURM_TMPDIR/CLEVR_v1.0.zip
-module load anaconda/3
-module unload cuda/11.2 && module load cuda/11.0
-deactivate
-act_bcd
+module load Anaconda/2021.05-nsc1
+module buildenv-gcccuda/11.4-8.3.1-bare  
+conda activate crl_env
 echo `date` "Python starting"
-echo "python run_decoder_bcd_across_interv.py --config defaults ${id} --data_seed ${seed} --exp_edges ${exp_edge} --lr ${lr} --num_steps ${step} --num_samples ${num_sample} --num_nodes ${num_nodes} --proj_dims ${proj_dims} --obs_data ${obs_data} --off_wandb ${off_wandb} --train_loss ${train_loss} --decoder_layers ${decoder_layers} --learn_L ${learn_L} --learn_P ${learn_P} --L_KL ${L_KL} --P_KL ${P_KL} --learn_noise ${learn_noise} --across_interv ${across_interv} --reg_decoder ${reg_decoder} --exp_name ${exp_name} --interv_type ${interv_type} --interv_value ${interv_value} --obs_Z_KL ${obs_Z_KL} --identity_proj ${identity_proj} --proj_sparsity ${proj_sparsity}"
+echo "python run_decoder_bcd_across_interv.py --config defaults ${id} --data_seed ${seed} --exp_edges ${exp_edge} --lr ${lr} --num_steps ${step} --num_samples ${num_sample} --num_nodes ${num_nodes} --proj_dims ${proj_dims} --obs_data ${obs_data} --off_wandb ${off_wandb} --train_loss ${train_loss} --decoder_layers ${decoder_layers} --learn_L ${learn_L} --learn_P ${learn_P} --L_KL ${L_KL} --P_KL ${P_KL} --learn_noise ${learn_noise} --across_interv ${across_interv} --reg_decoder ${reg_decoder}  --interv_type ${interv_type} --interv_value ${interv_value} --obs_Z_KL ${obs_Z_KL} --identity_proj ${identity_proj} --proj_sparsity ${proj_sparsity}"
 
 cd exps/decoder_bcd_exps
-python run_decoder_bcd_across_interv.py --config defaults ${id} --data_seed ${seed} --exp_edges ${exp_edge} --lr ${lr} --num_steps ${step} --num_samples ${num_sample} --num_nodes ${num_nodes} --proj_dims ${proj_dims} --obs_data ${obs_data} --off_wandb ${off_wandb} --train_loss ${train_loss} --decoder_layers ${decoder_layers} --learn_L ${learn_L} --learn_P ${learn_P} --L_KL ${L_KL} --P_KL ${P_KL} --learn_noise ${learn_noise} --across_interv ${across_interv} --reg_decoder ${reg_decoder} --exp_name ${exp_name} --interv_type ${interv_type} --interv_value ${interv_value} --obs_Z_KL ${obs_Z_KL} --identity_proj ${identity_proj} --proj_sparsity ${proj_sparsity}
+python run_decoder_bcd_across_interv.py --config defaults ${id} --data_seed ${seed} --exp_edges ${exp_edge} --lr ${lr} --num_steps ${step} --num_samples ${num_sample} --num_nodes ${num_nodes} --proj_dims ${proj_dims} --obs_data ${obs_data} --off_wandb ${off_wandb} --train_loss ${train_loss} --decoder_layers ${decoder_layers} --learn_L ${learn_L} --learn_P ${learn_P} --L_KL ${L_KL} --P_KL ${P_KL} --learn_noise ${learn_noise} --across_interv ${across_interv} --reg_decoder ${reg_decoder} --interv_type ${interv_type} --interv_value ${interv_value} --obs_Z_KL ${obs_Z_KL} --identity_proj ${identity_proj} --proj_sparsity ${proj_sparsity}
 cd ../..
 
 echo $end
